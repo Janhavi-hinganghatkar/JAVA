003d1f79fd2dbaaae476ac2cc60cce1e7b457c22
Janhavi-hinganghatkar <202401100025@mitaoe.ac.in>
2025-06-17 23:20:10 +0530

    Add files via upload

---
+++ SubtractionExample.java
@@ -0,0 +1,18 @@
+public class SubtractionExample {
+
+    // Function to subtract two numbers
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 15;
+        int num2 = 7;
+
+        // Calling the function and storing result
+        int result = subtract(num1, num2);
+
+        // Displaying the result
+        System.out.println("Subtraction Result: " + result);
+    }
+}
